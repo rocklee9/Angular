3e525ff0f9af471a23ed51c2f48698e7303124bd
Minh Hoang <minhhoang97hk@gmail.com>
2018-09-22 15:30:14 +0700

    add hero and villain

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './app/heroes/heroes.component';
+import { VillainComponent } from './app/villain/villain.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    VillainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app/entity/Villain.ts
@@ -0,0 +1,5 @@
+export class Villain{
+	public id:number;
+	public name:string;
+	public skills:string[];
+}
