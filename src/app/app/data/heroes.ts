6f1e64dec3581b96e1b0d77b31a3775fcc715d7d
Minh Hoang <minhhoang97hk@gmail.com>
2018-09-24 22:53:33 +0700

    commit buoi 3

--- src/app/app/data/heroes.ts
+++ src/app/app/data/heroes.ts
@@ -1,7 +1,7 @@
 import {Hero} from "../entity/Hero";
 
 export const listHero: Hero[]=[
-  {id:1,name:'đoàn dự',skills:['lăng ba vi bộ','lục mạch thần kiếm']},
-  {id:2,name:'tiêu phong',skills:['giáng long thập bát chưởng','đã cẩu bổng']},
-  {id:3,name:'trương vô kỵ',skills:['thái cực quyền','thái cực kiếm','càn khôn đại na di']}
+  {id:1,name:'Đoàn Dự',skills:['Lăng Ba Vi Bộ','Lục Mạch Thần Kiếm','Bắc Minh Thần Công']},
+  {id:2,name:'Tiêu Phong',skills:['Giáng Long Thập Bát Chưởng','Đã Cẩu Bổng']},
+  {id:3,name:'Trương Vô Kỵ',skills:['Thái Cực Quyền','Thái Cực Kiếm','Càn Khôn Đại Na Di','Cửu Dương Chân Kinh']}
 ]
